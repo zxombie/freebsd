22dc3e8b5805fc777ebea139978d654f8c72584e
Andrew Turner <andrew@fubar.geek.nz>
2018-09-14 11:37:52 +0000

    Implement d_mmap_single
    This allows us to use cdev private data again, meaning we can open
    the device multiple times.

--- sys/kern/kern_kcov.c
+++ sys/kern/kern_kcov.c
@@ -42,18 +42,26 @@ __FBSDID("$FreeBSD$");
 #include <sys/malloc.h>
 #include <sys/mman.h>
 #include <sys/proc.h>
+#include <sys/rwlock.h>
 #include <sys/stat.h>
 #include <sys/sx.h>
 #include <sys/sysctl.h>
 #include <sys/systm.h>
 #include <sys/types.h>
 
+#include <vm/vm.h>
+#include <vm/vm_extern.h>
+#include <vm/vm_object.h>
+#include <vm/vm_page.h>
+#include <vm/vm_pager.h>
+
 #include <machine/cpufunc.h>
 
 #include <vm/pmap.h>
 
 MALLOC_DEFINE(M_KCOV_INFO, "kcovinfo", "KCOV info type");
-MALLOC_DEFINE(M_KCOV_BUF, "kcovbuffer", "KCOV buffer type");
+
+#define	KCOV_ELEMENT_SIZE	sizeof(uint64_t)
 
 /*
  * - Only move away from the running state in the current thread. This is to
@@ -71,16 +79,19 @@ typedef enum {
 } kcov_state_t;
 
 struct kcov_info {
-	uint64_t	*buf;
+	vm_object_t	bufobj;
+	vm_offset_t	kvaddr;
 	size_t		size;
+	size_t		bufsize;
 	kcov_state_t	state;
 	int		mode;
+	bool		mmap;
 };
 
 /* Prototypes */
 static d_open_t		kcov_open;
 static d_close_t	kcov_close;
-static d_mmap_t		kcov_mmap;
+static d_mmap_single_t	kcov_mmap_single;
 static d_ioctl_t	kcov_ioctl;
 
 void __sanitizer_cov_trace_pc(void);
@@ -101,7 +112,7 @@ static struct cdevsw kcov_cdevsw = {
 	.d_version =	D_VERSION,
 	.d_open =	kcov_open,
 	.d_close =	kcov_close,
-	.d_mmap =	kcov_mmap,
+	.d_mmap_single = kcov_mmap_single,
 	.d_ioctl =	kcov_ioctl,
 	.d_name =	"kcov",
 };
@@ -151,7 +162,7 @@ __sanitizer_cov_trace_pc(void)
 {
 	struct thread *td;
 	struct kcov_info *info;
-	uint64_t index;
+	uint64_t *buf, index;
 
 	/*
 	 * To guarantee curthread is properly set, we exit early
@@ -171,16 +182,18 @@ __sanitizer_cov_trace_pc(void)
 	if (info->mode != KCOV_MODE_TRACE_PC)
 		return;
 
-	KASSERT(info->buf != NULL,
+	KASSERT(info->kvaddr != 0,
 	    ("__sanitizer_cov_trace_pc: NULL buf while running"));
 
+	buf = (uint64_t *)info->kvaddr;
+
 	/* The first entry of the buffer holds the index */
-	index = info->buf[0];
-	if (index + 2 >= info->size / sizeof(info->buf[0]))
+	index = buf[0];
+	if (index + 2 >= info->size / KCOV_ELEMENT_SIZE)
 		return;
 
-	info->buf[index + 1] = (uint64_t)__builtin_return_address(0);
-	info->buf[0] = index + 1;
+	buf[index + 1] = (uint64_t)__builtin_return_address(0);
+	buf[0] = index + 1;
 }
 
 static bool
@@ -188,7 +201,7 @@ trace_cmp(uint64_t type, uint64_t arg1, uint64_t arg2, uint64_t ret)
 {
 	struct thread *td;
 	struct kcov_info *info;
-	uint64_t index;
+	uint64_t *buf, index;
 
 	/*
 	 * To guarantee curthread is properly set, we exit early
@@ -208,21 +221,23 @@ trace_cmp(uint64_t type, uint64_t arg1, uint64_t arg2, uint64_t ret)
 	if (info->mode != KCOV_MODE_TRACE_CMP)
 		return (false);
 
-	KASSERT(info->buf != NULL,
+	KASSERT(info->kvaddr != 0,
 	    ("__sanitizer_cov_trace_pc: NULL buf while running"));
 
+	buf = (uint64_t *)info->kvaddr;
+
 	/* The first entry of the buffer holds the index */
-	index = info->buf[0];
+	index = buf[0];
 
 	/* Check we have space to store all elements */
-	if (index * 4 + 5 >= info->size / sizeof(info->buf[0]))
+	if (index * 4 + 5 >= info->size / KCOV_ELEMENT_SIZE)
 		return (false);
 
-	info->buf[index * 4 + 1] = type;
-	info->buf[index * 4 + 2] = arg1;
-	info->buf[index * 4 + 3] = arg2;
-	info->buf[index * 4 + 4] = ret;
-	info->buf[0] = index + 1;
+	buf[index * 4 + 1] = type;
+	buf[index * 4 + 2] = arg1;
+	buf[index * 4 + 3] = arg2;
+	buf[index * 4 + 4] = ret;
+	buf[0] = index + 1;
 
 	return (true);
 }
@@ -329,39 +344,48 @@ __sanitizer_cov_trace_switch(uint64_t val, uint64_t *cases)
 			return;
 }
 
+static void
+kcov_mmap_cleanup(void *arg)
+{
+	struct kcov_info *info = arg;
+
+	KASSERT(info->state != KCOV_STATE_RUNNING,
+	    ("kcov_mmap_cleanup: Cleanup while running"));
+
+	if (info->kvaddr != 0) {
+		pmap_qremove(info->kvaddr, info->bufsize / PAGE_SIZE);
+		kva_free(info->kvaddr, info->bufsize);
+	}
+	if (info->bufobj != NULL && !info->mmap)
+		vm_object_deallocate(info->bufobj);
+	free(info, M_KCOV_INFO);
+}
+
 static int
 kcov_open(struct cdev *dev, int oflags, int devtype, struct thread *td)
 {
 	struct kcov_info *info;
+	int error;
 
-	/*
-	 * TODO: We could use devfs_set_cdevpriv, but it needs care around
-	 * mmap. For now only allow a single user to open the device at a
-	 * time.
-	 */
 	info = malloc(sizeof(struct kcov_info), M_KCOV_INFO, M_ZERO | M_WAITOK);
 	info->state = KCOV_STATE_OPEN;
 	info->mode = -1;
+	info->mmap = false;
 
-	mtx_lock(&kcov_mtx);
-	if (dev->si_drv1 != NULL) {
-		mtx_unlock(&kcov_mtx);
-		free(info, M_KCOV_INFO);
-		return (EBUSY);
-	}
+	if ((error = devfs_set_cdevpriv(info, kcov_mmap_cleanup)) != 0)
+		kcov_mmap_cleanup(info);
 
-	dev->si_drv1 = info;
-	mtx_unlock(&kcov_mtx);
-
-	return (0);
+	return (error);
 }
 
 static int
 kcov_close(struct cdev *dev, int fflag, int devtype, struct thread *td)
 {
 	struct kcov_info *info;
+	int error;
 
-	info = dev->si_drv1;
+	if ((error = devfs_get_cdevpriv((void **)&info)) != 0)
+		return (error);
 
 	KASSERT(info != NULL, ("kcov_close with no kcov_info structure"));
 
@@ -369,49 +393,66 @@ kcov_close(struct cdev *dev, int fflag, int devtype, struct thread *td)
 	if (info->state == KCOV_STATE_RUNNING)
 		return (EBUSY);
 
-	dev->si_drv1 = NULL;
-	atomic_thread_fence_seq_cst();
-
-	free(info->buf, M_KCOV_BUF);
-	free(info, M_KCOV_INFO);
-
 	return (0);
 }
 
 static int
-kcov_mmap(struct cdev *dev, vm_ooffset_t offset, vm_paddr_t *paddr,
-    int prot, vm_memattr_t *memattr __unused)
+kcov_mmap_single(struct cdev *dev, vm_ooffset_t *offset, vm_size_t size,
+    struct vm_object **object, int nprot)
 {
 	struct kcov_info *info;
+	int error;
 
-	if (prot & PROT_EXEC)
+	if ((nprot & (PROT_EXEC | PROT_READ | PROT_WRITE)) !=
+	    (PROT_READ | PROT_WRITE))
 		return (EINVAL);
 
-	info = dev->si_drv1;
+	if ((error = devfs_get_cdevpriv((void **)&info)) != 0)
+		return (error);
 
-	if (info->buf == NULL || offset < 0 || offset >= info->size)
+	if (info->kvaddr == 0 || size != info->size)
 		return (EINVAL);
 
-	*paddr = vtophys(info->buf) + offset;
+	info->mmap = true;
+	*offset = 0;
+	*object = info->bufobj;
 	return (0);
 }
 
 static int
 kcov_alloc(struct kcov_info *info, size_t size)
 {
-	size_t buf_size;
+	size_t n, pages;
+	vm_page_t *m;
 
-	KASSERT(info->buf == NULL, ("kcov_alloc: Already have a buffer"));
+	KASSERT(info->kvaddr == 0, ("kcov_alloc: Already have a buffer"));
 	KASSERT(info->state == KCOV_STATE_OPEN,
 	    ("kcov_alloc: Not in open state (%x)", info->state));
 
-	if (size < 2 * sizeof(info->buf[0]) || size > kcov_max_size)
+	if (size < 2 * KCOV_ELEMENT_SIZE || size > kcov_max_size)
 		return (EINVAL);
 
 	/* Align to page size so mmap can't access other kernel memory */
-	buf_size = roundup2(size, PAGE_SIZE);
+	info->bufsize = roundup2(size, PAGE_SIZE);
+	pages = info->bufsize / PAGE_SIZE;
 
-	info->buf = malloc(buf_size, M_KCOV_BUF, M_ZERO | M_WAITOK);
+	if ((info->kvaddr = kva_alloc(info->bufsize)) == 0)
+		return (ENOMEM);
+
+	info->bufobj = vm_pager_allocate(OBJT_PHYS, 0, info->bufsize,
+	    PROT_READ | PROT_WRITE, 0, curthread->td_ucred);
+
+	m = malloc(sizeof(*m) * pages, M_TEMP, M_WAITOK);
+	VM_OBJECT_WLOCK(info->bufobj);
+	for (n = 0; n < pages; n++) {
+		m[n] = vm_page_grab(info->bufobj, n,
+		    VM_ALLOC_NOBUSY | VM_ALLOC_ZERO | VM_ALLOC_WIRED);
+		m[n]->valid = VM_PAGE_BITS_ALL;
+	}
+	VM_OBJECT_WUNLOCK(info->bufobj);
+	pmap_qenter(info->kvaddr, m, pages);
+	free(m, M_TEMP);
+
 	info->size = size;
 
 	return (0);
@@ -424,9 +465,8 @@ kcov_ioctl(struct cdev *dev, u_long cmd, caddr_t data, int fflag __unused,
 	struct kcov_info *info;
 	int mode, error;
 
-	info = dev->si_drv1;
-
-	error = 0;
+	if ((error = devfs_get_cdevpriv((void **)&info)) != 0)
+		return (error);
 
 	switch (cmd) {
 	case KIOSETBUFSIZE:
