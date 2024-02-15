0a630aaecfbf5b72c3255de7292328703be6fc25
Andrew Turner <andrew@FreeBSD.org>
2024-02-15 16:17:42 +0000

    arm64: If the HCR_E2H field is stuck stay in EL2
    This allows us to remove the hack where we check if the HCR_E2H field
    is set in has_hyp().
    
    Sponsored by:	Arm Ltd

--- sys/arm64/arm64/machdep.c
+++ sys/arm64/arm64/machdep.c
@@ -207,12 +207,7 @@ pan_enable(void)
 bool
 has_hyp(void)
 {
-
-	/*
-	 * XXX The E2H check is wrong, but it's close enough for now.  Needs to
-	 * be re-evaluated once we're running regularly in EL2.
-	 */
-	return (boot_el == 2 && (hcr_el2 & HCR_E2H) == 0);
+	return (boot_el == 2);
 }
 
 bool
